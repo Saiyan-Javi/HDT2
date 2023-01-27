84d3cd2dc4acb0ac2a670051f7be68996bd4d06d
Dan <98132259+Dan090807@users.noreply.github.com>
2023-01-27 08:20:22 -0600

    Create PostfixCalculator.java

---
+++ Model/PostfixCalculator.java
@@ -0,0 +1,15 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class PostfixCalculator implements IPostfixCalculator {
+
+    @Override
+    public int Calculate(ArrayList postfix_expression) throws Exception {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    
+    
+}
