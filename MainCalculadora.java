a57a15b80c481923533b9bfabc4f5c385b63a26c
Saiyan-Javi <111992322+Saiyan-Javi@users.noreply.github.com>
2023-01-28 19:11:14 -0600

    Add files via upload

---
+++ MainCalculadora.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MainCalculadora {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String menu1 = "\n ************** Bienvenido a nuestra calculadora ***********";
+        String opc1;
+
+        System.out.println(menu1);
+        opc1 = teclado.nextLine();
+        teclado.nextLine(); ///Aqui ingresa la direccion del archivo 
+    }
+}
+            
