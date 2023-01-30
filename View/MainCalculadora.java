aa51dc579978e35498fedd8aede7b3538b9e1caa
Dan <98132259+Dan090807@users.noreply.github.com>
2023-01-30 12:22:58 -0600

    Removed DriverTest

---
+++ View/MainCalculadora.java
@@ -0,0 +1,57 @@
+package View;
+import java.util.Scanner;
+
+import javax.print.DocFlavor.STRING;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
+import Model.IPostfixCalculator;
+import Model.PostfixCalculator;
+
+public class MainCalculadora {
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+        ArrayList<String> operaciones = new ArrayList<String>();
+
+        IPostfixCalculator<String> miCalculadora = new PostfixCalculator();
+
+
+        String menu1 = "\n ************** Bienvenido a nuestra calculadora ***********";
+        String opc1;
+
+        System.out.println(menu1);
+        opc1 = teclado.nextLine();
+        teclado.nextLine(); ///Aqui ingresa la direccion del archivo 
+        teclado.close();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(opc1));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                operaciones.add(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("No se ha encontrado el archivo a leer. Por favor, agrege uno al directorio.");
+        }
+
+        miCalculadora.Calculate(operaciones);
+        
+        // Esto es el case para los números 
+
+        ///char caracter = '@'; aqui deberia estar la lista de los números y que vaya verifiando cada uno de ellos 
+      /*  int representacion = new operaciones;
+        
+        if (representacion <= 57 && representacion >= 48) {
+            //toma los número de la pila y los almacena 
+        } else if (representacion == 42 ||  representacion == 43 || representacion == 45 || representacion == 47) {
+            //cuenta como operador y toma 2 valores 
+        }*/
+        
+    }
+}
+            
