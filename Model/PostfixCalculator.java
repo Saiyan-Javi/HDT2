0e54e5204f95bdae7b9028f90cba14a83f6d63d6
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2023-01-30 14:32:34 -0600

    Moved the check for the operations to start checking when it finds a symbol

--- Model/PostfixCalculator.java
+++ Model/PostfixCalculator.java
@@ -25,41 +25,35 @@ public class PostfixCalculator<T> implements IPostfixCalculator<T> {
         for (T t : Data) {
             try {
                 int a = Integer.parseInt(t.toString());
-                System.out.print("Este dígito es " + a + "\n");
+                System.out.print("Este dígito es " + a + "\n");  //Esto es para testeo
             } catch (NumberFormatException e) {
-                System.out.println("Halle el símbolo " + t + "\n");
+                System.out.println("Halle el símbolo " + t + "\n"); //Esto es para testeo
                 //ES SíMBOLO
+                switch(t.toString()) {  //TODO: Cambiar los métodos para realizar las operaciones.
+        
+                    case "+": {
+                        int suma = (int) Data.get(0) + (int) Data.get(1);
+                        res = suma;
+                    } break;
+        
+                    case "-": {
+                        int resta = (int) Data.get(0) - (int) Data.get(1);
+                        res = resta;
+                    } break;
+        
+                    case "*": {
+                        int mul = (int) Data.get(0) * (int) Data.get(1);
+                        res = mul;
+                    } break;
+        
+                    case "/": {
+                        int div = (int) Data.get(0) / (int) Data.get(1);
+                        res = div;
+                    } break;
+                }
             }
         }
 
-        /*switch( Data.get(0)) {
-
-            case 48:{
-                //Data.add(T);
-            } break;
-
-            case 43: {
-                int suma = (int) Data.get(0) + (int) Data.get(1);
-                res = suma;
-            } break;
-
-            case 45: {
-                int resta = (int) Data.get(0) - (int) Data.get(1);
-                res = resta;
-            } break;
-
-            case 42: {
-                int mul = (int) Data.get(0) * (int) Data.get(1);
-                res = mul;
-            } break;
-
-            case 47: {
-                int div = (int) Data.get(0) / (int) Data.get(1);
-                res = div;
-            } break;
-
-        }
-        /
         /* Estos son los remanentes de cómo lo había hecho antes. */
 
         /*if((int) Data.get(0) == 43)  {
